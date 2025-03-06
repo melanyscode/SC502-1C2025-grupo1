af512a827b415f4920cbd1c634e8dffd9e32cb2b
Melany <123525805+melanyscode@users.noreply.github.com>
2025-03-05 19:13:57 -0600

    cambio minimo
    se agregaron los colores en el css, estilos para los a (links) en css y esta en proceso la parte de contacto

--- views/index.php
+++ views/index.php
@@ -3,16 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titulo </title>
+    <title>Contacto</title>
     <?php include "links.php";?>  
 </head>
-
 <body>
-    
-    <div class="header">
-    <?php include_once "navbar.php"; ?>
-   
-    </div>
+    <?php include_once("navbar.php"); ?>
 </body>
-
 </html>
