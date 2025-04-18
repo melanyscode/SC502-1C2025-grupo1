3075276f9e89709ad7a624c31364d3b75ef4f643
Melany <123525805+melanyscode@users.noreply.github.com>
2025-04-17 18:17:03 -0600

    mvc

--- app/session/getSesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-header('Content-Type: application/json');
-
-if (isset($_SESSION['user'])) {
-  echo json_encode($_SESSION['user']);
-} else {
-  echo json_encode(['error' => 'no-session']);
-}
